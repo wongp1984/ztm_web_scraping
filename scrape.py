f22b64d79c7b3f61b1ae5970a81be0708c057f62
wongp1984 <wongp.1984@gmail.com>
2022-08-22 12:28:37 +0100

    added a simple scrape module using beautiful soup to scrape hacker news

---
+++ scrape.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get('https://news.ycombinator.com/')
+soup = BeautifulSoup(res.text, 'html.parser')
+# print(soup.find_all('div'))
+
+# print(soup.find_all('a'))
+# print(soup.find('a'))
+
+# print(soup.select('.score'))
+
+# print(soup.select('#score_32496381'))
+links = soup.select('.titlelink')
+subtext = soup.select('.subtext')
+# print(votes)
+# print(votes[0].get('id'))
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 50:
+                hn.append({'title': title, 'link': href, 'votes': points} )
+    return sort_stories_by_votes(hn)
+
+pprint.pprint(create_custom_hn(links, subtext))
+
